7fba83a6af3423bc73dd9b122bac022b875131f5
Michael <mikhzobby@gmail.com>
2023-07-02 07:56:35 +0100

    Made modifications

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,6 +1,7 @@
 # A simple insertion sort implementation 
 
-def insertion_sort(array: list):
+def insertion_sort_1(array: list):
+    # Non-decreasing order
     for j in range(1, len(array)):
         i = j - 1
         key = array[j]
@@ -12,5 +13,19 @@ def insertion_sort(array: list):
 
 test_array = [4, 2, 1, 19, 11, 0]
 
-print(insertion_sort(test_array))
+print(insertion_sort_1(test_array))
+
+def insertion_sort_2(array: list):
+    # Non-increasing order
+    for j in range(1, len(array)):
+        key = array[j]
+        i = j - 1
+        while i > -1 and array[i] < key:
+            array[i + 1] = array[i]
+            i -= 1
+        array[i+1] = key
+    return array
+
+print(insertion_sort_2(test_array))
+    
 
