5daebfd0a19c8ee9b129878fa8b3ba706d8e1f95
Michael <mikhzobby@gmail.com>
2023-07-08 05:51:17 +0100

    Basic formatting

--- merge_sort.py
+++ merge_sort.py
@@ -1,6 +1,8 @@
 def mergesort(array: list):
+
     if len(array) <= 1:
         return array
+    
     mid = len(array) // 2
     left = array[:mid]
     right = array[mid:]
@@ -12,7 +14,9 @@ def mergesort(array: list):
 
 
 def merge(left: list, right: list, array: list):
+
     i = j = k = 0
+
     while i < len(left) and j < len(right):
         if left[i] <= right[j]:
             array[k] = left[i]
@@ -21,14 +25,17 @@ def merge(left: list, right: list, array: list):
             array[k] = right[j]
             j += 1
         k += 1
+
     while i < len(left):
         array[k] = left[i]
         i += 1
         k += 1
+
     while j < len(right):
         array[k] = right[j]
         j += 1
         k += 1
+        
     return array
 
 array_1 = [19, 2, 1, 0, 4, 5, 6, 9, 10]
