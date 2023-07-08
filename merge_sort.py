7b947cf9bd954ec0479c680c0b20171a61aed3c1
Michael <mikhzobby@gmail.com>
2023-07-08 05:50:05 +0100

    optimized memory usage

--- merge_sort.py
+++ merge_sort.py
@@ -5,29 +5,31 @@ def mergesort(array: list):
     left = array[:mid]
     right = array[mid:]
 
-    left = mergesort(left)
-    right = mergesort(right)
+    mergesort(left)
+    mergesort(right)
 
-    return merge(left, right)
+    return merge(left, right, array)
 
 
-def merge(left: list, right: list):
-    sorted_list = []
-    i = j = 0
+def merge(left: list, right: list, array: list):
+    i = j = k = 0
     while i < len(left) and j < len(right):
         if left[i] <= right[j]:
-            sorted_list.append(left[i])
+            array[k] = left[i]
             i += 1
         else:
-            sorted_list.append(right[j])
+            array[k] = right[j]
             j += 1
+        k += 1
     while i < len(left):
-        sorted_list.append(left[i])
+        array[k] = left[i]
         i += 1
+        k += 1
     while j < len(right):
-        sorted_list.append(right[j])
+        array[k] = right[j]
         j += 1
-    return sorted_list
+        k += 1
+    return array
 
 array_1 = [19, 2, 1, 0, 4, 5, 6, 9, 10]
 
